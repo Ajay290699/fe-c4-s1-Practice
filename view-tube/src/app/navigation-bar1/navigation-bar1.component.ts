551808cf45adb27af7251a954b7f73a7c12d92b2
Ajay Chavan <ajayc9838@gmail.com>
2022-12-12 09:06:22 +0530

    completed

---
+++ view-tube/src/app/navigation-bar1/navigation-bar1.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-navigation-bar1',
+  templateUrl: './navigation-bar1.component.html',
+  styleUrls: ['./navigation-bar1.component.css']
+})
+export class NavigationBar1Component {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
